f793c7cfe71aa8bb5be23a10ca33612e459ab8cb
jwno55 <kobe@insterior.co.kr>
2022-02-09 08:19:58 +0900

    홈바 아이콘 추가

--- pages/_app.js
+++ pages/_app.js
@@ -21,20 +21,18 @@ export default function MyApp({ Component, pageProps }) {
         <title>SRAPP</title>
         <link rel="manifest" href="/manifest.json" />
         <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
         <link rel='shortcut icon' href='/favicon.ico' />
-        <link rel='apple-touch-icon' href='/icons/icon-512x512.png' />
-        <link rel='apple-touch-icon' sizes='152x152' href='/icons/icon-512x512.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/icon-512x512.png' />
-        <link rel='apple-touch-icon' sizes='167x167' href='/icons/icon-512x512.png' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='2048x2732' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='2048x2732' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='1668x2224' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='1536x2048' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='1125x2436' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='1242x2208' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='750x1334' />
-        <link rel='apple-touch-startup-image' href='/images/splash_test.jpg' sizes='640x1136' />
+        <link rel='apple-touch-icon' href='/icons/icon_512x512.png' />
+        <link rel='apple-touch-icon' sizes='152x152' href='/icons/icon_152x152.png' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/icons/icon_180x180.png' />
+        <link rel='apple-touch-icon' sizes='167x167' href='/icons/icon_167x167.png' />
+        <link rel="apple-touch-startup-image" href="/images/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"/>
+        <link rel="apple-touch-startup-image" href="/images/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"/>
+        <link rel="apple-touch-startup-image" href="/images/splash-1242x2208.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"/>
+        <link rel="apple-touch-startup-image" href="/images/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"/>
+        <link rel="apple-touch-startup-image" href="/images/splash-1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"/>
+        <link rel="apple-touch-startup-image" href="/images/splash-1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"/>
+        <link rel="apple-touch-startup-image" href="/images/splash-2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"/>
       </Head>
       <BookProvider>
         <Component {...pageProps} />
