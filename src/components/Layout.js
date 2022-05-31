f1a94cf82aade73757b010b10ec51ea86113d80a
jwno55 <kobe@insterior.co.kr>
2022-05-31 13:15:41 +0900

    no message

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import HomeBar from '../../src/components/HomeBar';
 
 export default function Layout({ children }) {
-    const homeBarPages = ["Home", "Sermonmain", "Onmain", "Post", "faith"];
+    const adminPages = ["Admin", "Enter", "Weekly", "WeeklyCreate"];
 
     useEffect(() => {
         Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
@@ -11,8 +11,7 @@ export default function Layout({ children }) {
     return (
         <>
             {children}
-            {/* { (adminPages && !adminPages.includes(children.type.name)) && <HomeBar />} */}
-            { (homeBarPages && homeBarPages.includes(children.type.name)) && <HomeBar />}
+            { (adminPages && !adminPages.includes(children.type.name)) && <HomeBar />}
         </>
     )
 }
