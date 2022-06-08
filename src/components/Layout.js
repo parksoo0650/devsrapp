c39b2c41c057219f8274d935e28aac8c1aa9015c
jwno55 <kobe@insterior.co.kr>
2022-06-08 11:25:48 +0900

    no message

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,13 @@ export default function Layout({ children }) {
     useEffect(() => {
         Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
         // setTimeout(scrollTo, 0, 0, 100);
-        window.addEventListener("load", function () {
-            // Set a timeout...
-            setTimeout(function () {
-                // Hide the address bar!
-                window.scrollTo(0, 10);
-            }, 0);
-        });
+        // window.addEventListener("load", function () {
+        //     // Set a timeout...
+        //     setTimeout(function () {
+        //         // Hide the address bar!
+        //         window.scrollTo(0, 10);
+        //     }, 0);
+        // });
     }, []);
 
     return (
