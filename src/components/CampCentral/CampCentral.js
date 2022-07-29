4aa4b3f8c188d07f954676df48d6194423a8cfcd
lsg001008 <lsg001008@gmail.com>
2022-07-29 21:45:30 +0900

    :hammer: 차량운행 페이지 수정

--- src/components/CampCentral/CampCentral.js
+++ src/components/CampCentral/CampCentral.js
@@ -1,7 +1,11 @@
 import CampHeader from '../CampHeader/CampHeader';
 
 const CampCentral = ({ onClick }) => {
-  return <CampHeader onClick={onClick} title='진행본부' />;
+  return (
+    <>
+      <CampHeader onClick={onClick} title="진행본부" />
+    </>
+  );
 };
 
 export default CampCentral;
--- src/components/CampHome/CampHome.js
+++ src/components/CampHome/CampHome.js
@@ -14,17 +14,17 @@ const CampInfo = () => {
     router.push(`2022-summer-camp/${event.target.id}`);
   };
 
-  const icons = document.querySelectorAll('.camp-home-menu-icon');
-  console.dir(icons);
+  // const icons = document.querySelectorAll('.camp-home-menu-icon');
+  // console.dir(icons);
 
   return (
     <div className={cn('CampHome')}>
-      <CampMenu onClick={onClick} title='수련회 일정표' id='schedule' />
-      <CampMenu onClick={onClick} title='프로그램 세부정보' id='events' />
-      <CampMenu onClick={onClick} title='식당안내' id='meal' />
-      <CampMenu onClick={onClick} title='기도실(리더센터) 사용안내' id='room' />
-      <CampMenu onClick={onClick} title='차량운행' id='shuttle' />
-      <CampMenu onClick={onClick} title='진행본부' id='central' />
+      <CampMenu onClick={onClick} title="수련회 일정표" id="schedule" />
+      <CampMenu onClick={onClick} title="프로그램 세부정보" id="events" />
+      <CampMenu onClick={onClick} title="식당안내" id="meal" />
+      <CampMenu onClick={onClick} title="기도실(리더센터) 사용안내" id="room" />
+      <CampMenu onClick={onClick} title="차량운행" id="shuttle" />
+      <CampMenu onClick={onClick} title="진행본부" id="central" />
       {/* 지도 CampMap */}
       {/* 환영글 CampWelcome */}
     </div>
