01dc5884e6d96383a1f5f3f291c0da217a7cd813
lsg001008 <lsg001008@gmail.com>
2022-07-30 19:21:20 +0900

    :hammer: 프로그램 버튼 숨김

--- src/components/CampProgram/CampProgram.js
+++ src/components/CampProgram/CampProgram.js
@@ -19,12 +19,12 @@ const CampProgram = () => {
 
   return (
     <div className={cn('CampProgram')}>
-      <CampMenu onClick={onClick} title='수련회 일정표' id='schedule' />
+      {/* <CampMenu onClick={onClick} title='수련회 일정표' id='schedule' /> */}
       <CampMenu onClick={onClick} title='프로그램 세부정보' id='events' />
-      <CampMenu onClick={onClick} title='식당안내' id='meal' />
-      <CampMenu onClick={onClick} title='기도실(리더센터) 사용안내' id='room' />
+      {/* <CampMenu onClick={onClick} title='식당안내' id='meal' /> */}
+      {/* <CampMenu onClick={onClick} title='기도실(리더센터) 사용안내' id='room' /> */}
       <CampMenu onClick={onClick} title='차량운행' id='shuttle' />
-      <CampMenu onClick={onClick} title='진행본부' id='central' />
+      {/* <CampMenu onClick={onClick} title='진행본부' id='central' /> */}
       {/* 지도 CampMap */}
       {/* 환영글 CampWelcome */}
 
