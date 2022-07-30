39d7afed19440631cc833448c4e7fe141037329c
jeamyung.lee <jeamyung.lee@homeplus.co.kr>
2022-07-30 22:07:12 +0900

    no message

--- src/components/CampHome/subComponent/SungrakInstaItem.js
+++ src/components/CampHome/subComponent/SungrakInstaItem.js
@@ -6,16 +6,16 @@ const cn = classNames.bind(styles);
 const SungrakInstaItem = ({ author, preview, image }) => {
   return (
     <article className={cn("SungrakInstaItem")}>
-      <div>
+      <div className={cn("imgbox")}>
         <img
           className={cn("image")}
           src={`https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/${image}/shorts`}
         />
       </div>
-      <p>
+      <div className={cn("textbox")}>
         <div className={cn("author")}>{author}</div>
         <div className={cn("preview")}>{preview}</div>
-      </p>
+      </div>
     </article>
   );
 };
