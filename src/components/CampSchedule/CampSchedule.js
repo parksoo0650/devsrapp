bf681f086c35ec1233107c0d5ed05ad2a352e07d
lsg001008 <lsg001008@gmail.com>
2022-08-02 10:00:16 +0900

    :hammer: 여름수련회 프로그램 작성

--- src/components/CampSchedule/CampSchedule.js
+++ src/components/CampSchedule/CampSchedule.js
@@ -24,46 +24,57 @@ const CampSchedule = ({ onClick }) => {
       </nav>
 
       <div className={cn('flexBox')}>
-        {/* 시간 표시 */}
-        <div className={cn('Timetable')}>
-          <p>06</p>
-          <p>07</p>
-          <p>08</p>
-          <p>09</p>
-          <p>10</p>
-          <p>11</p>
-          <p>12</p>
-          <p>13</p>
-          <p>14</p>
-          <p>15</p>
-          <p>16</p>
-          <p>17</p>
-          <p>18</p>
-          <p>19</p>
-          <p>20</p>
-          <p>21</p>
-          <p>22</p>
-        </div>
+        {/* 시간 표시 자리 왼쪽 빈 공간*/}
+        <div />
 
         {/* 일정 표시 */}
         <div className={cn('ScheduleTable')}>
-          <div className={cn('DawnCrusade')}>새벽성회</div>
-          <div className={cn('Breakfast')}>아침식사</div>
-          <div className={cn('Rest1')}>휴식</div>
-          <div className={cn('HappyLecture')}>행복한 특강</div>
-          <div className={cn('LectureForEveryone')}>전교인 특강</div>
-          <div className={cn('Lunch')}>점심식사</div>
-          <div className={cn('Rest2')}>휴식</div>
-          <div className={cn('Recreation')}>
+          <div className={cn('item', 'DawnCrusade')}>새벽성회</div>
+          <div className={cn('item', 'Breakfast')}>아침식사</div>
+          <div className={cn('item', 'Rest1')}>휴식</div>
+          <div className={cn('item', 'HappyLecture')}>행복한 특강</div>
+          <div className={cn('item', 'LectureForEveryone')}>전교인 특강</div>
+          <div className={cn('item', 'Lunch')}>점심식사</div>
+          <div className={cn('item', 'Rest2')}>휴식</div>
+          <div className={cn('item', 'Recreation')}>
             행복 더하기 <br /> (레크레이션)
           </div>
-          <div className={cn('HealingCrusade')}>신유집회</div>
-          <div className={cn('SungrakCinema')}>성락시네마</div>
-          <div className={cn('Rest3')}>휴식</div>
-          <div className={cn('Dinner')}>저녁식사</div>
-          <div className={cn('Rest4')}>휴식</div>
-          <div className={cn('EveningCrusade')}>저녁성회</div>
+          <div className={cn('item', 'HealingCrusade')}>신유집회</div>
+          <div className={cn('item', 'SungrakCinema')}>성락시네마</div>
+          <div className={cn('item', 'Rest3')}>휴식</div>
+          <div className={cn('item', 'Dinner')}>저녁식사</div>
+          <div className={cn('item', 'Rest4')}>휴식</div>
+          <div className={cn('item', 'EveningCrusade')}>저녁성회</div>
+
+          <div className={cn('blank1')} />
+          <div className={cn('blank2')} />
+          <div className={cn('blank3')} />
+          <div className={cn('blank4')} />
+          <div className={cn('blank5')} />
+          <div className={cn('blank6')} />
+          <div className={cn('blank7')} />
         </div>
+      </div>
+
+      {/* 시간 표시 */}
+      <div className={cn('Timetable')}>
+        <p>06</p>
+        <p>07</p>
+        <p>08</p>
+        <p>09</p>
+        <p>10</p>
+        <p>11</p>
+        <p>12</p>
+        <p>13</p>
+        <p>14</p>
+        <p>15</p>
+        <p>16</p>
+        <p>17</p>
+        <p>18</p>
+        <p>19</p>
+        <p>20</p>
+        <p>21</p>
+        <p>22</p>
       </div>
     </div>
   );
