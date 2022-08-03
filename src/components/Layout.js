f14e21e89c4a2a6ede3330cb4ddb1647cfe7736b
jeamyung.lee <jeamyung.lee@homeplus.co.kr>
2022-08-04 08:52:24 +0900

    뱃지테스트

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
+import PWABadge from "pwa-badge";
 
 export default function Layout({ children }) {
-    useEffect(() => {
-        Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
-    }, []);
+  useEffect(() => {
+    Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
 
-    return (
-        <>
-            {children}
-        </>
-    )
+    // Create an Instance
+    const badge = new PWABadge();
+
+    if (badge.isSupported()) {
+      // Hoora!, Supports the Badge feature
+      console.log("yes");
+      badge.syncSetBadge(1);
+    } else {
+      // Does not supports
+      console.log("no");
+    }
+  }, []);
+
+  return <>{children}</>;
 }
