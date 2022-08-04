b93c6c2971f3a4b2753975aea819663a75cf8139
jeamyung.lee <jeamyung.lee@homeplus.co.kr>
2022-08-04 09:34:56 +0900

    뱃지 테스트 종료

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,13 @@
-import React, { useState, useEffect } from "react";
-import PWABadge from "pwa-badge";
+import React, { useState, useEffect } from "react";ㅋ
 
 export default function Layout({ children }) {
-  useEffect(() => {
-    Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+    useEffect(() => {
+        Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+    }, []);
 
-    // Create an Instance
-    const badge = new PWABadge();
-
-    if (badge.isSupported()) {
-      //   console.log("yes");
-      //   badge.syncSetBadge(1);
-      //   badge.syncClearBadge();
-    } else {
-      //   console.log("no");
-    }
-  }, []);
-
-  return <>{children}</>;
+    return (
+        <>
+            {children}
+        </>
+    )
 }
