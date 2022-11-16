425f71d337a1b00e506200cac85b19b1078ad66c
lsg001008 <lsg001008@gmail.com>
2022-11-16 17:48:45 +0900

    :hammer: 미래세대 콘텐츠 템플릿 추가

--- src/components/Home/DepartmentBody.jsx
+++ src/components/Home/DepartmentBody.jsx
@@ -1,8 +1,15 @@
+import DepartmentContent from './DepartmentContent';
+
 export default function DepartmentBody({ department }) {
   return (
-    <span className='p-[30px] text-xl text-[#333333]'>
-      <strong>[{department}] </strong>
-      콘텐츠 보여줄 자리입니다...
-    </span>
+    <>
+      <span className='p-[30px] text-xl text-[#333333]'>
+        <strong>[{department}] </strong>
+        콘텐츠 보여줄 자리입니다...
+      </span>
+
+      <DepartmentContent />
+      <DepartmentContent />
+    </>
   );
 }
---
+++ src/components/Home/DepartmentContent.jsx
@@ -0,0 +1,16 @@
+export default function DepartmentContent() {
+  return (
+    <li key='' onClick={() => {}} className='px-[30px] mb-7'>
+      {/* <img src='' /> */}
+
+      <div className='w-full h-[176px] bg-slate-200' />
+
+      <h3 className='pt-3.5 text-base'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </h3>
+
+      <div className='pt-3 text-xs'>2022.00.00</div>
+    </li>
+  );
+}
