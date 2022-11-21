8510fbe7ac9e03e78a728c8722eb47575f58f4a7
lsg001008 <lsg001008@gmail.com>
2022-11-21 22:14:18 +0900

    :hammer: 피드 템플릿 생성
    - tempData를 이용하여 템플릿 생성.

--- pages/feed.js
+++ pages/feed.js
@@ -1,9 +1,74 @@
+import Feed from '../src/components/Feed';
 import HomeBar from '../src/components/HomeBar';
+
+const tempData = [
+  {
+    date: '2022.11.02',
+    title:
+      '2022 교회와 함께! 성락인과 함께! 헌신 참여 챌린지 - 「No Skip송」 #shorts',
+    tags: ['노스킵', '어린이부', '헌신'],
+    department: '성락교회 어린이부',
+  },
+  {
+    date: '2022.11.02',
+    title:
+      '2022 교회와 함께! 성락인과 함께! 헌신 참여 챌린지 - 「No Skip송」 #shorts',
+    tags: ['노스킵', '어린이부', '헌신'],
+    department: '성락교회 어린이부',
+  },
+  {
+    date: '2022.11.02',
+    title:
+      '2022 교회와 함께! 성락인과 함께! 헌신 참여 챌린지 - 「No Skip송」 #shorts',
+    tags: ['노스킵', '어린이부', '헌신'],
+    department: '성락교회 어린이부',
+  },
+  {
+    date: '2022.10.31',
+    title:
+      '2022 교회와 함께! 성락인과 함께! 헌신 참여 챌린지 - 「No Skip송」 #shorts',
+    tags: ['노스킵', '어린이부', '헌신'],
+    department: '성락교회 어린이부',
+  },
+  {
+    date: '2022.10.31',
+    title:
+      '2022 교회와 함께! 성락인과 함께! 헌신 참여 챌린지 - 「No Skip송」 #shorts',
+    tags: ['노스킵', '어린이부', '헌신'],
+    department: '성락교회 어린이부',
+  },
+];
 
 export default function feed() {
   return (
-    <>
+    <section>
+      <Feed.Tags />
+
+      {tempData.map((currentContent, index, contents) => (
+        <>
+          {/* 새로운 날짜 표시. 동일한 날짜 생략. */}
+          {contents[index - 1]?.date == currentContent.date || (
+            <Feed.Date date={currentContent.date} />
+          )}
+
+          <Feed.Content
+            key={index}
+            date={currentContent.date}
+            title={currentContent.title}
+            tags={currentContent.tags}
+            department={currentContent.department}
+          />
+        </>
+      ))}
+
+      {/* blank */}
+      <div
+        style={{
+          height: '82px',
+        }}
+      />
+
       <HomeBar />
-    </>
+    </section>
   );
 }
---
+++ src/components/Feed/index.js
@@ -0,0 +1,52 @@
+/**
+ * 피드 최상단 태그.
+ */
+Feed.Tags = () => {
+  const tags = ['노스킵', '어린이부', '헌신'];
+
+  return (
+    <div className='px-5 py-7'>
+      {tags.map((tag) => (
+        <span className='inline-block text-[22px] font-semibold mr-3'>
+          #{tag}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+/**
+ * 피드 날짜.
+ */
+Feed.Date = ({ date }) => {
+  return <h3 className='font-semibold text-[20px] px-5 mb-3'>{date}</h3>;
+};
+
+/**
+ * 피드 콘텐츠.
+ */
+Feed.Content = ({ date, title, tags, department }) => {
+  return (
+    <div className='px-5'>
+      {/* 영상 썸네일로 교체해야 합니다. */}
+      <div className='w-full h-[188px] bg-black mb-[14px]' />
+
+      <span className='block text-base mb-3'>{title}</span>
+
+      {tags.map((tag) => (
+        <span
+          className='inline-block text-[#444444] text-[14px] 
+          px-[10px] py-[2px] mr-[10px] border border-[#d4d4d4] rounded'
+        >
+          #{tag}
+        </span>
+      ))}
+
+      <span className='block py-3 mb-7 text-xs'>
+        {date} ﹒ {department}
+      </span>
+    </div>
+  );
+};
+
+export default function Feed() {}
