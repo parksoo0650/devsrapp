ed2c972540bb4b151590ec60b259a337437cbf2a
lsg001008 <lsg001008@gmail.com>
2022-11-25 23:54:28 +0900

    :lipstick: 스타일 수정
    - 주중 콘텐츠 요일 선택 탭 하단 border 추가.

--- src/components/Home/WeekdaySelectTab.jsx
+++ src/components/Home/WeekdaySelectTab.jsx
@@ -2,15 +2,15 @@ export default function WeekdaySelectTab({ weeks, setWeeks }) {
   const weekday = ['월', '화', '수', '목', '금'];
 
   return (
-    <ul className='flex pt-[50px] px-[30px] pb-[30px] justify-between'>
+    <ul className='flex pt-[40px] px-[30px] pb-[22px] justify-between'>
       {weekday.map((day, index) => (
         <li
           key={index}
           onClick={() => setWeeks(day)}
           className={
             weeks == day
-              ? 'text-base font-[600] text-black'
-              : 'text-base font-[600] text-[#a0a0a0]'
+              ? 'pb-2 text-base font-[600] text-black border-b-2 border-black'
+              : 'pb-2 text-base font-[600] text-[#a0a0a0]'
           }
         >
           {day}요일
