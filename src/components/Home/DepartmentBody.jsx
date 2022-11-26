4eabdf7ae3629ba9f937856cdca04ecc86602cb9
lsg001008 <lsg001008@gmail.com>
2022-11-26 15:59:39 +0900

    :fire: 코드 삭제
    - 불필요한 코드 삭제.

--- src/components/Home/DepartmentBody.jsx
+++ src/components/Home/DepartmentBody.jsx
@@ -2,23 +2,21 @@ import DepartmentContent from './DepartmentContent';
 import useSWR from 'swr';
 
 export default function DepartmentBody({ department }) {
-
   const { data: dataFeed } = useSWR('/api/contents?kind=feed');
   let dspContentsCnt = 0;
 
   return (
     <>
-      <span className='p-[30px] text-xl text-[#333333]'>
-        <strong>[{department}] </strong>
-      </span>
+      {/* blank */}
+      <div className='h-6' />
 
       {dataFeed?.contents.map((doc, i) => {
-        if(department == "전체"){
-          if(doc.subKind != "noskip") {
-            if(dspContentsCnt > 1) return;
+        if (department == '전체') {
+          if (doc.subKind != 'noskip') {
+            if (dspContentsCnt > 1) return;
             dspContentsCnt++;
-            return(
-              <DepartmentContent 
+            return (
+              <DepartmentContent
                 key={doc.id}
                 doc={doc}
                 imageSrc={`https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/${doc.image}/public`}
@@ -26,8 +24,8 @@ export default function DepartmentBody({ department }) {
             );
           }
         }
-        if(doc.subKind == department){
-          return(
+        if (doc.subKind == department) {
+          return (
             <DepartmentContent
               key={doc.id}
               doc={doc}
@@ -36,7 +34,6 @@ export default function DepartmentBody({ department }) {
           );
         }
       })}
-
     </>
   );
 }
