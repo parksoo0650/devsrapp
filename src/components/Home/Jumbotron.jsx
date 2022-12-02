079c452299590acce5d19bc95a13f858ef6a1186
lsg001008 <lsg001008@gmail.com>
2022-12-02 23:00:57 +0900

    :ambulance: 긴급 수정
    - 테스트 코드로 배포한 것 수정.

--- src/components/Home/Jumbotron.jsx
+++ src/components/Home/Jumbotron.jsx
@@ -99,8 +99,7 @@ export default function Jumbotron({ liveDatas }) {
               className='text-white font-[500] text-[32px] mb-[18px] max-h-[80px]
               tracking-[-0.5px] break-keep max-w-xs leading-10 text-ellipsis overflow-hidden'
             >
-              {/* {titleKr} */}
-              예수와 하나님은 하나이시다
+              {titleKr}
             </p>
 
             <p className='text-white text-[14px] leading-6'>
