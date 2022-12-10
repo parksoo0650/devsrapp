8563db1c84b307fa6fb689f81f08a044d5c9fc24
lsg001008 <lsg001008@gmail.com>
2022-12-10 18:33:26 +0900

    :alembic: 테스트
    - ClickToMovePage 컴포넌트 테스트.

---
+++ pages/test.js
@@ -0,0 +1,20 @@
+import ClickToMovePage from '../src/components/molecule/ClickToMovePage';
+
+export default function test() {
+  return (
+    <>
+      <ClickToMovePage
+        route='/'
+        content={
+          <div className='flex items-center justify-between px-7 py-5 bg-[#d38730]'>
+            <span className='text-base font-medium text-white'>
+              교회를 위한 기도제목 바로가기
+            </span>
+
+            <div className='w-4 h-4 bg-[url("/icons/right_arrow_white.svg")] bg-no-repeat bg-contain bg-center' />
+          </div>
+        }
+      />
+    </>
+  );
+}
