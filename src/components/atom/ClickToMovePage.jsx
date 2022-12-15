a9ec7bad59ad781582041ba200bba51f8cee5a0e
lsg001008 <lsg001008@gmail.com>
2022-12-15 18:39:59 +0900

    :bulb: 주석 추가
    - ClickToMovePage 컴포넌트 주석 추가.

--- src/components/atom/ClickToMovePage.jsx
+++ src/components/atom/ClickToMovePage.jsx
@@ -1,6 +1,12 @@
 import { useRouter } from 'next/router';
-import Button from '../atom/Button';
+import Button from './Button';
 
+/**
+ * 클릭 시 페이지 이동.
+ *
+ * @param {string} route 클릭 시 이동할 경로.
+ * @param {*} content 컴포넌트 텍스트 또는 콘텐츠.
+ */
 export default function ClickToMovePage({ route, content }) {
   const router = useRouter();
 
