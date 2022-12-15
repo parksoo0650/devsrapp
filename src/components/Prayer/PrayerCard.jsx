e90fac5d53bb9adc859f14f173f12de93383ba54
lsg001008 <lsg001008@gmail.com>
2022-12-15 15:22:13 +0900

    :hammer: 코드 수정
    - PrayerCard 항목 6-10 추가.
    - '교회와 감독님을 위한 기도제목' 추가.

--- src/components/Prayer/PrayerCard.jsx
+++ src/components/Prayer/PrayerCard.jsx
@@ -5,6 +5,11 @@ export default function PrayerCard({
   content3,
   content4,
   content5,
+  content6,
+  content7,
+  content8,
+  content9,
+  content10,
 }) {
   return (
     <div className='px-5'>
@@ -13,6 +18,36 @@ export default function PrayerCard({
       <p className='pb-7 leading-7 text-base'>
         · {content1} <br />· {content2} <br />· {content3} <br />· {content4}
         <br />· {content5}
+        {content6 && (
+          <>
+            <br />
+            <span>· {content6}</span>
+          </>
+        )}
+        {content7 && (
+          <>
+            <br />
+            <span>· {content7}</span>
+          </>
+        )}
+        {content8 && (
+          <>
+            <br />
+            <span>· {content8}</span>
+          </>
+        )}
+        {content9 && (
+          <>
+            <br />
+            <span>· {content9}</span>
+          </>
+        )}
+        {content10 && (
+          <>
+            <br />
+            <span>· {content10}</span>
+          </>
+        )}
       </p>
     </div>
   );
