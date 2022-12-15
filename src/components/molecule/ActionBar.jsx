dbe8c8c711737b12e0e58d9742d5cf3ee01f7bf7
lsg001008 <lsg001008@gmail.com>
2022-12-15 19:12:45 +0900

    :hammer: 코드 수정
    - 확장성을 고려하여 prop 이름 변경. (title -> center)
    - height prop 추가. 기본값은 10.

--- src/components/molecule/ActionBar.jsx
+++ src/components/molecule/ActionBar.jsx
@@ -1,28 +1,27 @@
 /**
  * 액션 바(툴바).
  *
- * @param {string} title 액션 바 중앙 제목.
  * @param {*} left 액션 바 왼쪽 툴.
+ * @param {*} center 액션 바 중앙 툴.
  * @param {*} right 액션 바 오른쪽 툴.
  */
-export default function ActionBar({ left, title, right }) {
-  const ACTION_BAR_HEIGHT = 10;
+export default function ActionBar({ left, center, right, height = 10 }) {
   const BACKGROUND_OPTIONS = 'bg-center bg-contain bg-no-repeat';
 
   return (
     <>
       <div
-        className={`px-4 w-full h-${ACTION_BAR_HEIGHT} fixed flex items-center justify-between bg-white`}
+        className={`px-4 w-full h-${height} fixed flex items-center justify-between bg-white z-50`}
       >
         <div className={`w-5 ${BACKGROUND_OPTIONS}`}>{left}</div>
 
-        <h2 className='text-base font-semibold text-[#333333]'>{title}</h2>
+        <h2 className='text-base font-semibold text-[#333333]'>{center}</h2>
 
         <div className={`w-5 ${BACKGROUND_OPTIONS}`}>{right}</div>
       </div>
 
       {/* blank */}
-      <div className={`h-${ACTION_BAR_HEIGHT}`} />
+      <div className={`h-${height}`} />
     </>
   );
 }
