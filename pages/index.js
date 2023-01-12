9a17c50e391a002af620b384caede73006db2087
lsg001008 <lsg001008@gmail.com>
2023-01-12 21:35:47 +0900

    :recycle: 코드 리팩토링
    - 인덱스 페이지 정리: 비즈니스 로직을 각각의 컴포넌트 내부로 이동.

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper';
-import useSWR from 'swr';
 import Jumbotron from '../src/components/Home/Jumbotron';
+import ListRow from '../src/components/molecule/ListRow';
 import WeekdayContent from '../src/components/Home/WeekdayContent';
 import QuickMenu from '../src/components/Home/QuickMenu';
 import Praise from '../src/components/Home/Praise';
 import Department from '../src/components/Home/Department';
 import HomeBar from '../src/components/HomeBar';
-import ListRow from '../src/components/molecule/ListRow';
-
-SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 export default function Home() {
-  const { data: dataSermon } = useSWR('/api/contents?kind=sermon');
-  const { data: dataOncontents } = useSWR('/api/contents?kind=oncontents');
-
-  const date = new Date();
-  const hours = new Date().getHours();
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [isLive, setIsLive] = useState(false);
-  const [liveDatas, setLiveDatas] = useState({
-    videoId: '',
-    title: '',
-    thumbnails: '',
-    publishedAt: '',
-    subKind: '',
-  });
-
-  const getLiveData = async () => {
-    const videoTitle = '';
-    const videoDateStr = '';
-    const thumbnails = '';
-    const videoIdStr = '';
-    const subKind = '';
-
-    videoTitle = dataSermon?.contents[0]?.name;
-    videoDateStr = dataSermon?.contents[0]?.publishedAt;
-    videoIdStr = dataSermon?.contents[0]?.videoId;
-    thumbnails = dataSermon?.contents[0]?.image;
-    subKind = dataSermon?.contents[0]?.subKind;
-
-    if (date.getDay() == 0 && hours > 6 && hours < 13) {
-      setIsLive(true);
-    }
-
-    setLiveDatas({
-      videoId: videoIdStr,
-      title: videoTitle,
-      thumbnails: thumbnails,
-      publishedAt: videoDateStr,
-      subKind: subKind,
-    });
-
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getLiveData();
-  }, [dataSermon, dataOncontents]);
-
   return (
     <>
-      {/* 좌측 상단 성락교회 로고 (헤더) */}
-      {/* <AppHeader isLive={isLive} dataSermon={dataSermon} /> */}
-
       {/* 대형 슬라이드 */}
-      <Jumbotron liveDatas={liveDatas} isLive={isLive} />
+      <Jumbotron />
 
+      {/* 교회 기도제목 */}
       <ListRow.V1
         title='교회를 위한 기도제목 바로가기'
         backgroundColor='#d38730'
@@ -77,10 +20,7 @@ export default function Home() {
       />
 
       {/* 주중 콘텐츠 */}
-      <WeekdayContent liveDatas={liveDatas} dataOncontents={dataOncontents} />
-
-      {/* 이번 주 설교 */}
-      {/* <SermonThisWeek liveDatas={liveDatas} dataSermon={dataSermon} /> */}
+      <WeekdayContent />
 
       {/* 퀵 메뉴 7개 */}
       <QuickMenu />
