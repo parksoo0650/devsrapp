9edc88686ceb654910571787a7bb6f6a4f18ecff
lsg001008 <lsg001008@gmail.com>
2023-01-22 22:58:50 +0900

    :art: 코드 정렬 - useUser 코드 가독성 향상.

--- libs/client/useUser.js
+++ libs/client/useUser.js
@@ -1,15 +1,23 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import useSWR from "swr";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
 export default function useUser() {
-    // const { data, error } = useSWR("/api/users/me", fetcher);
-    const { data, error } = useSWR("/api/users/me");
-    const router = useRouter();
-    useEffect(() => {
-        if (data && !data.ok) {
-            router.replace("/admin/Enter");
-        }
-    }, []);
-    return { user: data?.profile, isLoading: !data && !error };
+  /**
+   * 현재 클라이언트가 로그인 상태인지 확인.
+   */
+  const { data, error } = useSWR('/api/users/me');
+
+  const router = useRouter();
+
+  /**
+   * 로그인 상태가 아니면 로그인 페이지로 이동.
+   */
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace('/admin/Enter');
+    }
+  }, []);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
