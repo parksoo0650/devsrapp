2bcedc5dfd9b2b82fe373e84420722e1c97ba8cd
lsg001008 <lsg001008@gmail.com>
2023-01-23 19:08:24 +0900

    :hammer: 코드 수정 - AdminLayout 디자인 및 route 방식 수정. - 기존 컴포넌트를 활용하여 AdminLayout 구성, 간략화.

--- src/components/AdminLayout.js
+++ src/components/AdminLayout.js
@@ -1,45 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { cls } from '../../libs/utils';
-import { useRouter } from "next/router";
+import ActionBar from '../components/molecule/ActionBar';
+import ClickToMoveBack from '../components/atom/ClickToMoveBack';
 
-export default function Layout({
-    title,
-    canGoBack,
-    children,
-}) {
-    const router = useRouter();
-    const onClick = () => {
-        router.back();
-    };
-    return (
-        <div className="w-full max-w-2xl mx-auto p-4">
-            <div
-                className={cls(
-                    !canGoBack ? "justify-center" : "",
-                    "bg-white w-full max-w-2xl text-lg font-medium py-3 text-gray-800 border-b top-0  flex items-center"
-                )}
-            >
-                {canGoBack ? (
-                    <button onClick={onClick}>
-                        <svg
-                            className="w-6 h-6"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 19l-7-7 7-7"
-                            ></path>
-                        </svg>
-                    </button>
-                ) : null}
-                {title ? <span>{title}</span> : null}
-            </div>
-            <div className="pt-6">{children}</div>
-        </div>
-    );
+export default function Layout({ title, children, canGoBack }) {
+  return (
+    <>
+      {/* 상단 바 */}
+      <ActionBar center={title || ''} left={<ClickToMoveBack route='prev' />} />
+
+      {/* 콘텐츠 리스트 */}
+      <div>{children}</div>
+    </>
+  );
 }
