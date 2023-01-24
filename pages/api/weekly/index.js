dd956176793049a12f50b150996c2f2e2c9eb2e8
lsg001008 <lsg001008@gmail.com>
2023-01-24 20:56:13 +0900

    :hammer: 코드 수정 - Weekly data 정렬 방식에 volume: desc 추가. - Weekly API 코드 가독성 향상.

--- pages/api/weekly/index.js
+++ pages/api/weekly/index.js
@@ -1,70 +1,67 @@
-import withHandler from "../../../libs/server/withHandler";
-import client from "../../../libs/server/client";
-import { withApiSession } from "../../../libs/server/withSession";
+import client from '../../../libs/server/client';
+import withHandler from '../../../libs/server/withHandler';
+import { withApiSession } from '../../../libs/server/withSession';
 
 async function handler(req, res) {
+  /**
+   * GET 방식인 경우.
+   */
+  if (req.method === 'GET') {
+    const weekly = await client.weekly.findMany({
+      orderBy: [{ volume: 'desc' }, { weekNo: 'desc' }],
+    });
 
-    if (req.method === "GET") {
-        const weekly = await client.weekly.findMany({
-            orderBy: [
-                {
-                    weekNo: 'desc',
-                },
-            ],
-        });
-        res.json({
-            ok: true,
-            weekly,
-        });
-    }
+    res.json({ ok: true, weekly });
+  }
 
-    if (req.method === "POST") {
-        const {
-            body: {
-                publishedAt,
-                volume,
-                weekNo,
-                bible,
-                bibleEN,
-                titleKR,
-                titleEN,
-                descriptionKR,
-                descriptionEN,
-                hymn1,
-                hymn2,
-                pray1,
-                pray2,
-            }
-        } = req;
+  /**
+   * POST 방식인 경우.
+   */
+  if (req.method === 'POST') {
+    const {
+      body: {
+        publishedAt,
+        volume,
+        weekNo,
+        bible,
+        bibleEN,
+        titleKR,
+        titleEN,
+        descriptionKR,
+        descriptionEN,
+        hymn1,
+        hymn2,
+        pray1,
+        pray2,
+      },
+    } = req;
 
-        const weekly = await client.weekly.create({
-            data: {
-                publishedAt,
-                volume: +volume,
-                weekNo: +weekNo,
-                bible,
-                bibleEN,
-                titleKR,
-                titleEN,
-                descriptionKR,
-                descriptionEN,
-                hymn1: +hymn1,
-                hymn2: +hymn2,
-                pray1,
-                pray2,
-            },
-        });
-        res.json({
-            ok: true,
-            weekly,
-        });
-    }
+    const weekly = await client.weekly.create({
+      data: {
+        publishedAt,
+        volume: +volume,
+        weekNo: +weekNo,
+        bible,
+        bibleEN,
+        titleKR,
+        titleEN,
+        descriptionKR,
+        descriptionEN,
+        hymn1: +hymn1,
+        hymn2: +hymn2,
+        pray1,
+        pray2,
+      },
+    });
+
+    res.json({ ok: true, weekly });
+  }
 }
 
 export default withApiSession(
-    withHandler({
-        methods: ["GET", "POST"],
-        handler,
-        isPrivate: false,
-    })
+  withHandler({
+    methods: ['GET', 'POST'],
+    handler,
+    isPrivate: false,
+  })
 );
