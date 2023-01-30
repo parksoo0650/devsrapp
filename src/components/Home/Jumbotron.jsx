58f86b1a8136794894ca59310d9f2dc234547308
lsg001008 <lsg001008@gmail.com>
2023-01-30 23:18:48 +0900

    :art: 코드 정렬 - Jumbotron을 하위 컴포넌트로 분리. - 각 하위 컴포넌트에 주석 추가.

--- src/components/Home/Jumbotron.jsx
+++ src/components/Home/Jumbotron.jsx
@@ -8,20 +8,116 @@ import Skeleton from '../molecule/Skeleton';
 import ParsingUtil from '../../utils/ParsingUtil';
 import DateUtil from '../../utils/DateUtil';
 
+/**
+ * 어플 메인 대형 스크린.
+ */
 export default function Jumbotron() {
   const router = useRouter();
 
+  /**
+   * 점보트론 높이 설정.
+   */
   const [height, setHeight] = useState(null);
   useEffect(() => {
     setHeight(() => window.innerWidth * 1.333325);
   }, []);
 
+  /**
+   * 설교 데이터를 가져와서 파싱.
+   */
   const { data } = useSWR('/api/contents?kind=sermon');
   const { fullName, publishedAt, videoId, category, title, scripture } =
     ParsingUtil.parseSermonData(data);
 
+  /**
+   * 스와이퍼 자동 넘김 설정.
+   */
   SwiperCore.use([Autoplay]);
 
+  /**
+   * 주일 설교.
+   */
+  function SundaySermon() {
+    return (
+      <div
+        className={`w-full h-full bg-no-repeat bg-cover bg-center bg-[url('images/jumbotron_sermon.png')] px-8`}
+        onClick={() => {
+          router.push(
+            `/sermondetail?vid=${videoId}&vtit=${fullName}&vdate=${publishedAt}&kind=${category}`,
+            '/sermondetail'
+          );
+        }}
+      >
+        <div className='absolute top-72'>
+          <LiveBadge />
+          <SermonTitle />
+          <SermonScriptureAndPreacher />
+        </div>
+      </div>
+    );
+  }
+
+  /**
+   * 라이브 뱃지.
+   */
+  function LiveBadge() {
+    return (
+      <>
+        {title ? (
+          <p
+            className={`text-xs text-white px-[11px] py-[5.5px] mb-3 inline-block rounded-[100px] font-semibold ${
+              DateUtil.isLive ? 'bg-[#D43030]/70' : 'bg-[#D48830]/70'
+            }`}
+          >
+            <span>{DateUtil.isLive ? '예배실황' : '주일예배'}</span>
+          </p>
+        ) : (
+          <Skeleton.Badge />
+        )}
+      </>
+    );
+  }
+
+  /**
+   * 설교 제목.
+   */
+  function SermonTitle() {
+    return (
+      <>
+        {title ? (
+          <p className='text-white font-extrabold text-[32px] mb-3 max-h-[80px] tracking-[-0.5px] break-keep max-w-xs leading-10 text-ellipsis overflow-hidden'>
+            {title}
+          </p>
+        ) : (
+          <Skeleton.Title />
+        )}
+      </>
+    );
+  }
+
+  /**
+   * 설교 본문 및 설교자.
+   */
+  function SermonScriptureAndPreacher() {
+    return (
+      <>
+        {scripture ? (
+          <p className='text-[#eeeeee] text-[14px] leading-6 font-bold'>
+            {scripture} <br />
+            김성현 감독님
+          </p>
+        ) : (
+          <Skeleton.Description />
+        )}
+      </>
+    );
+  }
+
+  /**
+   * 점보트론 슬라이드.
+   *
+   * 5초(delay: 5000)마다 자동으로 슬라이드 넘김.
+   */
   return (
     <Swiper
       className='w-screen'
@@ -34,59 +130,16 @@ export default function Jumbotron() {
         disableOnInteraction: false,
       }}
     >
-      {/* 주일 설교 */}
+      {/**
+       * 주일 설교.
+       */}
       <SwiperSlide className={`h-[${height}px]`}>
-        <div
-          className={`w-full h-full bg-no-repeat bg-cover bg-center
-          bg-[url('images/jumbotron_sermon.png')] px-8`}
-          onClick={() => {
-            router.push(
-              `/sermondetail?vid=${videoId}&vtit=${fullName}&vdate=${publishedAt}&kind=${category}`,
-              '/sermondetail'
-            );
-          }}
-        >
-          <div className='absolute top-72'>
-            {/* 라이브 뱃지 */}
-            {title ? (
-              <p
-                className={`text-xs text-white px-[11px] py-[5.5px] mb-3
-                inline-block rounded-[100px] font-semibold ${
-                  DateUtil.isLive ? 'bg-[#D43030]/70' : 'bg-[#D48830]/70'
-                }`}
-              >
-                <span>{DateUtil.isLive ? '예배실황' : '주일예배'}</span>
-              </p>
-            ) : (
-              <Skeleton.Badge />
-            )}
-
-            {/* 설교 제목 */}
-            {title ? (
-              <p
-                className='text-white font-extrabold text-[32px] mb-3 max-h-[80px]
-                tracking-[-0.5px] break-keep max-w-xs leading-10 text-ellipsis overflow-hidden'
-              >
-                {title}
-              </p>
-            ) : (
-              <Skeleton.Title />
-            )}
-
-            {/* 설교 본문 */}
-            {scripture ? (
-              <p className='text-[#eeeeee] text-[14px] leading-6 font-bold'>
-                {scripture} <br />
-                김성현 감독님
-              </p>
-            ) : (
-              <Skeleton.Description />
-            )}
-          </div>
-        </div>
+        <SundaySermon />
       </SwiperSlide>
 
-      {/* 크리스마스 배너 */}
+      {/**
+       * 크리스마스 배너.
+       */}
       <SwiperSlide>
         <img
           className='w-full bg-no-repeat bg-cover bg-center'
