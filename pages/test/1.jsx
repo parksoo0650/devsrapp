3acba9456e11264a694c34bd6881ebcd494fa23b
leafdesk <lsg001008@gmail.com>
2023-04-13 16:49:11 +0900

    :alembic: 코드 테스트 - TestPage1 내용 삭제.

--- pages/test/1.jsx
+++ pages/test/1.jsx
@@ -1,21 +1,20 @@
 import TopBar from '@/components/TopBar';
 import Image from 'next/image';
 
-function SearchIcon() {
-    return <Image src={'/icons/icon_search.svg'} width={48} height={48} />;
-}
 
-function AlarmIcon() {
-    return <Image src={'/icons/icon_alarm.svg'} width={48} height={48} />;
-}
 
 export default function TestPage1() {
     return (
         <>
-            <TopBar left1={<SearchIcon />}
-                    left2='주중말씀'
-                    right1={<AlarmIcon />}
-                    right2={<SearchIcon />} />
+            temp
+            {/*<TopBar left1={<SearchIcon />}*/}
+            {/*        left2="주중말씀"*/}
+            {/*        right1={<AlarmIcon />}*/}
+            {/*        right2={<SearchIcon />} />*/}
+            {/*<TopBar left1={<div className="pl-[14px]"><CiLogo /></div>}*/}
+            {/*        right1={<AlarmIcon />}*/}
+            {/*        right2={<SearchIcon />} />*/}
         </>
-    );
+    )
+        ;
 }
