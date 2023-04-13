a5307cd42ed53ec5643977e866efbc755278c2b4
leafdesk <lsg001008@gmail.com>
2023-04-13 16:47:54 +0900

    :hammer: 코드 수정 - Home에 TopBar.Home 컴포넌트 추가.

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,49 @@ import QuickMenu from '../src/components/Home/QuickMenu';
 import Praise from '../src/components/Home/Praise';
 import Department from '../src/components/Home/Department';
 import HomeBar from '../src/components/HomeBar';
+import TopBar from '@/components/TopBar';
 
 export default function Home() {
-  return (
-    <>
-      {/* 대형 슬라이드 */}
-      <Jumbotron />
+    return (
+        <>
+            <TopBar.Home />
 
-      {/* 교회 기도제목 */}
-      <ListRow.V1
-        title='교회를 위한 기도제목 바로가기'
-        backgroundColor='#d38730'
-        route='/prayer'
-      />
+            {/* 대형 슬라이드 */}
+            <Jumbotron />
 
-      {/* 주중 콘텐츠 */}
-      <WeekdayContent />
+            {/* 교회 기도제목 */}
+            <ListRow.V1
+                title="교회를 위한 기도제목 바로가기"
+                backgroundColor="#d38730"
+                route="/prayer"
+            />
 
-      {/* 퀵 메뉴 7개 */}
-      <QuickMenu />
+            {/* 주중 콘텐츠 */}
+            <WeekdayContent />
 
-      {/* 교회 표어 */}
-      <div className='mdbanner'>
-        <img src='/images/banner_23.jpg' />
-      </div>
+            {/* 퀵 메뉴 7개 */}
+            <QuickMenu />
 
-      {/* 은혜로운 찬양 */}
-      <Praise />
+            {/* 교회 표어 */}
+            <div className="mdbanner">
+                <img src="/images/banner_23.jpg" />
+            </div>
 
-      {/* 성락교회 미래세대 */}
-      <Department />
+            {/* 은혜로운 찬양 */}
+            <Praise />
 
-      {/* blank */}
-      <div
-        style={{
-          height: '82px',
-        }}
-      />
+            {/* 성락교회 미래세대 */}
+            <Department />
 
-      {/* 하단 메뉴 바 */}
-      <HomeBar />
-    </>
-  );
+            {/* blank */}
+            <div
+                style={{
+                    height: '82px',
+                }}
+            />
+
+            {/* 하단 메뉴 바 */}
+            <HomeBar />
+        </>
+    );
 }
