70db436d0f056d84e5db5327af9a2df0d58b5784
leafdesk <lsg001008@gmail.com>
2023-04-27 14:07:35 +0900

    :art: 코드 정렬 - Icon 컴포넌트 코드 가독성 개선. - 세미콜론 제거, 주석 추가 등.

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -4,14 +4,14 @@ export default function Icon({ children }) {
     return <>{children}</>
 }
 
-Icon.Search = () => <Image src={'/icons/icon_search.svg'} width={48} height={48} />
+Icon.Search = () => <Image src={'/icons/icon_search.svg'} width={48} height={48} />             // 검색.
 
-Icon.Alarm = () => <Image src={'/icons/icon_alarm.svg'} width={48} height={48} />
+Icon.Alarm = () => <Image src={'/icons/icon_alarm.svg'} width={48} height={48} />               // 알림.
 
-Icon.CiLogo = () => <Image src={'/images/ci_sungrak.svg'} width={124} height={37} />   // Church Identity Logo Image
+Icon.CiLogo = () => <Image src={'/images/ci_sungrak.svg'} width={124} height={37} />            // CI 로고.
 
-Icon.Back = () => <Image src={'/icons/icon_back.svg'} width={48} height={48} />
+Icon.Back = () => <Image src={'/icons/icon_back.svg'} width={48} height={48} />                 // 뒤로 가기.
 
-Icon.Copy = () => <Image src={'/icons/icon_copy.svg'} width={48} height={48} />
+Icon.Copy = () => <Image src={'/icons/icon_copy.svg'} width={48} height={48} />                 // 복사.
 
-Icon.Download = () => <Image src={'/icons/icon_download.svg'} width={48} height={48} />
+Icon.Download = () => <Image src={'/icons/icon_download.svg'} width={48} height={48} />         // 다운로드.
