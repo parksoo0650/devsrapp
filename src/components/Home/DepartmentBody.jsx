0abebf55b08fcaa0064b15556460c3541deb5e41
leafdesk <lsg001008@gmail.com>
2023-06-14 09:55:37 +0900

    :hammer: 코드 수정 - 홈 화면에서 주중 콘텐츠, 교회 표어 삭제. - '성락교회 미래세대'를 '오늘의 콘텐츠'로 변경.

--- src/components/Home/DepartmentBody.jsx
+++ src/components/Home/DepartmentBody.jsx
@@ -1,39 +1,41 @@
-import DepartmentContent from './DepartmentContent';
-import useSWR from 'swr';
+import DepartmentContent from './DepartmentContent'
+import useSWR from 'swr'
 
 export default function DepartmentBody({ department }) {
-  const { data: dataFeed } = useSWR('/api/contents?kind=feed');
-  let dspContentsCnt = 0;
+    const { data: dataFeed } = useSWR('/api/contents?kind=feed')
+    let dspContentsCnt = 0
 
-  return (
-    <>
-      {/* blank */}
-      <div className='h-6' />
+    console.log(dataFeed?.contents)
 
-      {dataFeed?.contents.map((doc, i) => {
-        if (department == '전체') {
-          if (doc.subKind != 'noskip') {
-            if (dspContentsCnt > 1) return;
-            dspContentsCnt++;
-            return (
-              <DepartmentContent
-                key={doc.id}
-                doc={doc}
-                imageSrc={`https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/${doc.image}/public`}
-              />
-            );
-          }
-        }
-        if (doc.subKind == department) {
-          return (
-            <DepartmentContent
-              key={doc.id}
-              doc={doc}
-              imageSrc={`https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/${doc.image}/public`}
-            />
-          );
-        }
-      })}
-    </>
-  );
+    return (
+        <>
+            {/* blank */}
+            <div className="h-6" />
+
+            {dataFeed?.contents.map((doc, i) => {
+                if (department === '전체') {
+                    if (doc.subKind !== 'noskip') {
+                        if (dspContentsCnt > 1) return
+                        dspContentsCnt++
+                        return (
+                            <DepartmentContent
+                                key={doc.id}
+                                doc={doc}
+                                imageSrc={`https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/${doc.image}/public`}
+                            />
+                        )
+                    }
+                }
+                if (doc.subKind === department) {
+                    return (
+                        <DepartmentContent
+                            key={doc.id}
+                            doc={doc}
+                            imageSrc={`https://imagedelivery.net/dnbl58MgrkUrjmB9YWa_dA/${doc.image}/public`}
+                        />
+                    )
+                }
+            })}
+        </>
+    )
 }
